a45c7651db4b1c1f793a0defdc310a03cce2abd5
seo722 <95078290+seo722@users.noreply.github.com>
2022-04-03 16:08:56 +0900

    router 변경

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -24,25 +25,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <Link to="/home">
-        <button>to home </button>
-      </Link>
+      <Home />
     </div>
   );
 }
