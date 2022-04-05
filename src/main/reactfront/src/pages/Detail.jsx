87cb924aaa50453357d7955df7c43bb911fa7679
qkxmaos <59613036+qkxmaos@users.noreply.github.com>
2022-04-05 18:01:10 +0900

    Revert "detail page"
    This reverts commit 876172e405195457b5cbd7554ad667ec499a563a.

--- src/main/reactfront/src/pages/Detail.jsx
+++ src/main/reactfront/src/pages/Detail.jsx
@@ -1,37 +1,11 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import styled from "styled-components";
+import React from "react";
 
 const Detail = () => {
-  const [detailWriting, setDetailWriting] = useState({
-    title: "제목",
-    content: "내용",
-  });
-  const param = useParams();
-  console.log(param.id);
-
-  const getDetailWriting = async () => {
-    const res = await axios.get(`/board/${param.id}`);
-    setDetailWriting(res);
-  };
-  useEffect(getDetailWriting, []);
-
   return (
-    <Wrapper>
-      <h1>{detailWriting.title}</h1>
-      <p>{detailWriting.content}</p>
-    </Wrapper>
+    <div>
+      <div>Detail</div>
+    </div>
   );
 };
 
 export default Detail;
-
-const Wrapper = styled.div`
-  background-color: powderblue;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
--- src/main/reactfront/src/pages/Home.jsx
+++ src/main/reactfront/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, NavLink, Route, useNavigate } from "react-router-dom";
+import {Link, NavLink, Route, useNavigate} from "react-router-dom";
 import styled from "styled-components";
 
 const Home = () => {
   const [list, setList] = useState([]);
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const getList = () => {
     axios
@@ -22,28 +22,25 @@ const Home = () => {
 
   const onWriteClick = async () => {
     const res = await axios.get("/board/new", null);
-    if (res.data) {
-      navigate("/write");
+    if(res.data){
+      navigate('/write')
     }
   };
 
-  const dummy = [
-    {
-      id: 2,
-      title: "title2",
-      writer: "writer2",
-    },
+  const dummy = [{
+    id: 2,
+    title: "title2",
+    writer: "writer2"
+  },
     {
       id: 3,
       title: "title3",
-      writer: "writer3",
-    },
-    {
+      writer: "writer3"
+    }, {
       id: 4,
       title: "title4",
-      writer: "writer4",
-    },
-  ];
+      writer: "writer4"
+    }]
 
   return (
     <Container>
@@ -59,17 +56,16 @@ const Home = () => {
           <tbody>
 <<<<<<< Updated upstream
             {dummy.map((list) => {
-              return (
-                <>
-                  <tr key={list.id}>
-                    <td>{list.id}</td>
-                    <td>
-                      <Link to={"/detail/" + list.id}>{list.title}</Link>
-                    </td>
-                    <td>{list.writer}</td>
-                  </tr>
-                </>
-              );
+              return(              <>
+                        <tr key={list.id}>
+                          <td>{list.id}</td>
+
+                            <td><Link to={"/detail/" + list.id}>{list.title}</Link></td>
+
+                          <td>{list.writer}</td>
+                        </tr>
+                      </>
+                  )
             })}
 =======
 >>>>>>> Stashed changes
@@ -117,7 +113,7 @@ const Home = () => {
           </tbody>
         </table>
 
-        <button onClick={onWriteClick}>글 쓰기</button>
+          <button onClick={onWriteClick}>글 쓰기</button>
       </Wrapper>
     </Container>
   );
